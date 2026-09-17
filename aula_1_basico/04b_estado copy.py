6dc4eb5adde3dcc59a6c8e9d0a8fdce6226da051
GabrielleDepoli <gabriellefernandesdepoli@gmail.com>
2026-09-17 11:59:38 -0300

    Finalização
    Finalização dos mini exercícios 4, 5, 6, 8, 9 e 10, orientados pelo professor (sem teste devido a problemas operacionais).

--- aula_1_basico/04b_estado copy.py
+++ aula_1_basico/04b_estado copy.py
@@ -1,29 +1,27 @@
 import flet as ft
 
 def main(page: ft.Page):
-    # Configurações iniciais
-    page.title="Modo Claro/Escuro"
-    #Inicia no modo Claro
+    page.title = "Modo Claro/Escuro"
     page.theme_mode = ft.ThemeMode.LIGHT
-    # Alinha tudo ao centro
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    
+    def alternar_tema(e):
+        page.theme_mode = (
+            ft.ThemeMode.DARK if page.theme_mode == ft.ThemeMode.LIGHT else ft.ThemeMode.LIGHT
+        )
+        construir_tela()
 
-    # Função que redesenha toda a tela se o tema for trocado
     def construir_tela():
-        # Limpa a tela antes de recriar o app
         page.controls.clear()
-
-        # Troca para o modo escuro
         escuro = page.theme_mode == ft.ThemeMode.DARK
-
-        # Define o ícone cores e texto de acordo com o tema atual
+        
         icone = ft.Icon(
             ft.Icons.LIGHT_MODE if escuro else ft.Icons.DARK_MODE,
             size=60,
             color=ft.Colors.AMBER if escuro else ft.Colors.BLUE_200,
         )
         texto = ft.Text(
-            "Modo Escuro ativado" if escuro else "Modo Claro ativado",
+            "Modo Escuro Ativado" if escuro else "Modo Claro Ativado",
             size=20,
             weight=ft.FontWeight.BOLD,
             color=ft.Colors.WHITE if escuro else ft.Colors.BLACK,
@@ -32,11 +30,9 @@ def main(page: ft.Page):
             "Ativar Modo Claro" if escuro else "Ativar Modo Escuro",
             on_click=alternar_tema,
         )
-
-        # Cor de fundo da tela
-        page.bgcolor= ft.Colors.BLACK if escuro else ft.Colors.WHITE
-
-        # Adiciona os elementos centralizados
+        
+        page.bgcolor = ft.Colors.BLACK if escuro else ft.Colors.WHITE
+        
         page.add(
             ft.Column(
                 [ft.Container(height=60), icone, texto, botao],
@@ -45,19 +41,9 @@ def main(page: ft.Page):
                 spacing=20,
             )
         )
-
         page.update()
 
-    def alternar_tema(e):
-        # Forma moderna de alternar (usando ternário)
-        page.theme_mode = (
-            ft.ThemeMode.DARK if page.theme_mode == ft.ThemeMode.LIGHT else ft.ThemeMode.LIGHT
-        )
+    construir_tela()
 
-        # Usando toda vez que usuário clicar para reconstruir
-        construir_tela()
-    construir_tela() # Chama a tela pela primeira vez
-
-# Inicia o aplicativo
-ft.app(target=main)
-
+# Chame a função oficial ft.app
+ft.app(target=main) 
